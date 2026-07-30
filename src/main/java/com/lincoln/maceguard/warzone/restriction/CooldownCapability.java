b08b86de1671db584ff090918734325820e701c4
P2wn <racecarboy77@gmail.com>
2026-07-30 10:24:47 -0400

    Add deterministic cooldown capability model

---
+++ src/main/java/com/lincoln/maceguard/warzone/restriction/CooldownCapability.java
@@ -0,0 +1,10 @@
+package com.lincoln.maceguard.warzone.restriction;
+
+/** Reliable server events that can confirm an item or ability was actually used. */
+public enum CooldownCapability {
+    PROJECTILE,
+    BLOCK_PLACE,
+    DIRECT_ATTACK,
+    LUNGE_EFFECT,
+    UNSUPPORTED
+}
